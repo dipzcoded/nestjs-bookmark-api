34f116a3a788b381d41734ea465c7ae29c8a67f8
dipzcoded <dipoakerele@gmail.com>
2022-10-01 21:29:50 +0100

    fix auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDto, SignUpDto } from './dtos';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body() signUpDto: SignUpDto): Promise<string> {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @Post()
+  async login(@Body() signInDto: SignInDto): Promise<string> {
+    return this.authService.login(signInDto);
+  }
+}
